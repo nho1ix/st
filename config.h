cda5965b798e47a2a8ed148811c13ba82031ffda
nho1ix <tupilbewb@gmail.com>
2021-02-11 00:53:54 -0800

    more fonts

--- config.h
+++ config.h
@@ -5,10 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Hermit:pixelsize=14:antialias=true:autohint=true";
-static char *font = "Source Code Pro Medium:pixelsize=15:antialias=true:autohint=true";
-// static char *font = "Fira Code:pixelsize=15:antialias=true:autohint=true";
-// static char *font = "SF Pro Display:style=Regular:pixelsize=15:antialias=true:autohint=true";
+//static char *font = "Hermit:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Source Code Pro Medium:size=12:antialias=true:autohint=true";
+//static char *font = "lemon:pixelsize=12:autohint=true";
+//static char *font = "JetbrainsMonoNL-Bold:pixelsize=14:antialias=true:autohint=true";
+//static char *font = "Fira Code:pixelsize=15:antialias=true:autohint=true";
+//static char *font = "SF Pro Display:style=Regular:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { "font-awesome:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 1;
 

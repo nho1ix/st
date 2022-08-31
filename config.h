658f025aeb380f4fe03de5fe267a2154a1680808
Victor Vo <tupilbewb@gmail.com>
2022-08-30 22:33:49 -0700

    Removed all commented lines and put into a separate config.h

--- config.h
+++ config.h
@@ -5,13 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Hermit:pixelsize=14:antialias=true:autohint=true";
-/* static char *font = "Source Code Pro Medium:size=12:antialias=true:autohint=true"; */
+
 static char *font = "JetBrainsMono Nerd Font:style:medium:pixelsize=15";
-//static char *font = "lemon:pixelsize=12:autohint=true";
-//static char *font = "JetbrainsMonoNL-Bold:pixelsize=14:antialias=true:autohint=true";
-//static char *font = "Fira Code:pixelsize=15:antialias=true:autohint=true";
-//static char *font = "SF Pro Display:style=Regular:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { "font-awesome:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 1;
 
@@ -25,6 +20,7 @@ static int borderpx = 1;
  */
 static char *shell = "/bin/sh";
 char *utmp = NULL;
+
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
